2e263020ddcd2f118f36a997eeb5e669d3670fbc
jin573 <rlawlstjs5573@gmail.com>
2026-01-18 00:54:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.44 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        //완주자 넣기 
-        //참여자 - 완주자? 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        Map<String, Integer> carr = new HashMap<>();
         
-        for(int i = 0; i<participant.length - 1; i++){
-            if(!participant[i].equals(completion[i])){
-                return participant[i];
+        //put
+        for(String s : completion){
+            carr.put(s, carr.getOrDefault(s, 0) + 1);
+        }
+        
+        //get
+        for(String s : participant){
+            if(carr.get(s) != null && carr.get(s) != 0){
+                carr.put(s, carr.get(s) - 1);
+            }else{
+                return s;
             }
         }
-       
-        return participant[participant.length-1];
+        
+        return "";
     }
 }
