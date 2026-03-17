9569e2fc6592b089e5af63208366128f75c23ff0
jin573 <rlawlstjs5573@gmail.com>
2026-03-17 18:51:52 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,35 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
-        int cnt = 0;
+        
+        //첫번째부터 방문(첫번째...마지막)//visited 되어있으면 pass
+            //본인이 아니고, 연결되어있으면 visited(첫번째...마지막 원소)
+            //더이상 연결된게 없으면 return
+            //횟수 카운트
+        int count = 0;
         for(int i = 0; i<n; i++){
             if(!visited[i]){
-                dfs(i, visited, computers);
-                cnt++;
+                dfs(visited, computers, i);
+                count++;
             }
         }
         
-        return cnt;
+        return count;
     }
     
-    private void dfs(int index, boolean[] visited, int[][] computers){
-        visited[index] = true;
+    private void dfs(boolean[] visited, int[][] computers, int i){
+        //방문함으로 변경하기 
+        if(visited[i] == true) return;
         
-        for(int i = 0; i<computers[index].length; i++){
-            if(index != i && computers[index][i] == 1 && !visited[i]){
-                dfs(i, visited, computers);
+        //본인이 속한 배열 가져오기
+        //그 안에서 본인 제외 확인하기 
+        
+        visited[i] = true; //본인은 true
+        for(int j = 0; j<computers[i].length; j++){
+            if(j != i && computers[i][j] == 1 && visited[j] == false){
+                dfs(visited, computers, j);
             }
         }
+        
     }
 }
