bd63da11dc292787b1e7d4453ec3e389bc5ec0ff
jin573 <rlawlstjs5573@gmail.com>
2026-03-27 20:48:57 +0900

    [level 2] Title: 피로도, Time: 3.17 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,24 @@
 class Solution {
-    int max = 0;
+    int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        
+        //나올 수 있는 경우의 수
+        //012 021 102 120 201 210
         boolean[] visited = new boolean[dungeons.length];
-        dfs(k, dungeons, visited, 0);
-        return max;
+        dfs(k, 0, visited, dungeons);
+        return answer;
     }
     
-    private void dfs(int k, int[][] dungeons, boolean[] visited, int count){
+    public void dfs(int k, int cnt, boolean[] visited, int[][] dungeons){
         
-        max = Math.max(max, count);
+        answer = Math.max(answer, cnt);
+        
         for(int i = 0; i<dungeons.length; i++){
-            if(visited[i]) continue;
-            if(k<dungeons[i][0]) continue;
+            if(!visited[i] && k >= dungeons[i][0]){
+                visited[i] = true;
+                dfs(k - dungeons[i][1], cnt+1, visited, dungeons);
+                visited[i] = false;
+            }
             
-            visited[i] = true;
-            dfs(k-dungeons[i][1], dungeons, visited, count+1);
-            visited[i] = false;
         }
     }
 }
