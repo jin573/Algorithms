9198f360e61ead2b437690b2226b09d3067cea0e
jin573 <rlawlstjs5573@gmail.com>
2026-04-06 00:39:17 +0900

    [level 3] Title: 여행경로, Time: 0.73 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,51 +1,38 @@
 import java.util.*;
-
 class Solution {
     boolean finished = false;
     public String[] solution(String[][] tickets) {
-        //i번 방문
-            //dfs
-                //언제 끝? 그 다음에 해당하는 a가 없을때
-                //돌아와서 다음 원소로 가야함 -> 따라서 정렬 필요
-        
-        boolean visited[] = new boolean[tickets.length];
         Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
-        String[] arr = new String[tickets.length+1];
+        boolean[] visited = new boolean[tickets.length];
+        ArrayList<String> arr = new ArrayList<>();
+        //icn으로 시작
+        arr.add("ICN");
+        dfs("ICN", tickets, visited, arr);
         
-        arr[0] = "ICN";
-        for(int i = 0; i<tickets.length; i++){
-            if(tickets[i][0].equals("ICN")){
-                visited[i] = true;
-                dfs(visited, tickets, i, 1, arr, 1);
-                visited[i] = false;
-            }
-        }
-        return arr;
+        return arr.toArray(new String[0]);
     }
     
-    public void dfs(boolean[] visited, String[][] tickets, int idx,
-                   int j, String[] arr, int depth){
-        if(finished){
-            return;
+    public void dfs(String target, String[][] tickets, boolean[] visited,
+                   ArrayList<String> arr){
+        //언제 종료?
+        //다 돌았을 때? 
+        if(arr.size() == tickets.length+1){
+            finished = true;
         }
         
-        arr[j] = tickets[idx][1];
-        if(depth == tickets.length) {
-            finished = true;
-            return;
-        }
         
         
         for(int i = 0; i<tickets.length; i++){
-            //반복문을 돌면서
-                //이번 값이 ticket[i][0] 와 같고 && 본인 차례가 아닌 경우
-                    //visited 하기
-            if(!visited[i] && tickets[idx][1].equals(tickets[i][0])){
+            //반복문을 돌면서 들어간 적 없고, 시작 경로에 해당하는 걸 찾음
+                //맞으면 target을 tickets[i][1] 로 변경함
+            if(!visited[i] && target.equals(tickets[i][0])){
                 visited[i] = true;
-                dfs(visited, tickets, i, j+1, arr, depth+1);
+                arr.add(tickets[i][1]);
+                dfs(tickets[i][1], tickets, visited, arr);
+                if(finished) return;
                 visited[i] = false;
+                arr.remove(arr.size() -1);
             }
         }
     }
-        
 }
