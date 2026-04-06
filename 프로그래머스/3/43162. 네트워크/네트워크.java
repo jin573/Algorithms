b0c5f6796678ef189dca0d0fe1e39681340a3e90
jin573 <rlawlstjs5573@gmail.com>
2026-04-06 23:44:39 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n, int[][] computers) {
+        //영역 -> 백트래킹 필요 없음
         boolean[] visited = new boolean[n];
         int count = 0;
         for(int i = 0; i<computers.length; i++){
             if(!visited[i]){
-                dfs(computers, i, visited);
+                dfs(i, visited, computers);
                 count++;
             }
         }
@@ -12,13 +13,14 @@ class Solution {
         return count;
     }
     
-    public void dfs(int[][] computers, int idx, boolean[] visited){
+    public void dfs(int idx, boolean[] visited, int[][] computers){
         visited[idx] = true;
-            
+        
         for(int i = 0; i<computers[idx].length; i++){
             if(!visited[i] && computers[idx][i] == 1){
-                dfs(computers, i, visited);
+                dfs(i, visited, computers);
             }
         }
+        
     }
 }
