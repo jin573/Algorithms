250327a7c7ac489b42027ba85aa569b9c60cee08
jin573 <rlawlstjs5573@gmail.com>
2026-04-07 00:26:24 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 10.69 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int m, int n, int[][] picture) {
+        //영역 -> 백트래킹x
+        //가장 큰 영역 -> int
         boolean[][] visited = new boolean[m][n];
-        int count=0;
+        int count = 0;
+        int size = 0;
         int max = 0;
-        int sum = 0;
-        //각 배열을 돌면서
-        //상하좌우 영역의 숫자가 같은 경우 count
+        
         for(int i = 0; i<picture.length; i++){
-            for(int j = 0; j<picture[i].length; j++){
+            for(int j = 0; j<picture[0].length; j++){
                 if(!visited[i][j] && picture[i][j] != 0){
-                    sum = dfs(i, j, visited, picture);
+                    size = dfs(i, j, picture, visited);
+                    
                     count++;
-                    max = Math.max(max, sum);
+                    max = Math.max(max, size);
                 }
             }
         }
@@ -21,28 +23,31 @@ class Solution {
         return new int[]{count, max};
     }
     
-    public int dfs(int x, int y, boolean[][] visited, int[][] picture){
+    public int dfs(int x, int y, int[][] picture, boolean[][] visited){
+        
+        visited[x][y] = true;
+        
+        int size = 1; 
+        //상하좌우에서 맞는 영역 가기
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
-        int sum = 1;
-        visited[x][y] = true;
         
         for(int i = 0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            
-            if(nx < 0 || nx >= picture.length 
-                   || ny <0 || ny >= picture[i].length){
-                    continue;
+        
+            if(nx < 0 || nx >= picture.length || 
+              ny < 0 || ny >= picture[0].length){
+                
+                continue;
             }
             
-            if(!visited[nx][ny]){
-                if(picture[nx][ny] == picture[x][y]){
-                    sum += dfs(nx, ny, visited, picture);
-                }
+            if(!visited[nx][ny] && picture[nx][ny] == picture[x][y]){
+            
+                size += dfs(nx, ny, picture, visited);
             }
         }
         
-        return sum;
+        return size;
     }
 }
