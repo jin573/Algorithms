4d646bb692a9d01deea1b0ad81f4327c623646a1
jin573 <rlawlstjs5573@gmail.com>
2026-04-07 23:48:08 +0900

    [level 2] Title: 타겟 넘버, Time: 5.58 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,28 @@
 class Solution {
-    int count = 0;
     public int solution(int[] numbers, int target) {
-    //마지막에 합을 구하고 비교 -> 맞으면 count++
-        //아니면 백트래킹
+        //다 더한게 target 이랑 같으면 -> count 올리기
+        //아니면 -> 그냥 pass
+        //매번 숫자를 더할 sum 값
+        //그리고 그 숫자가 다른 부호일 때? 도 계산
         
-        dfs(numbers, target, 0 + numbers[0], 1);
-        dfs(numbers, target, 0 - numbers[0], 1);
-        return count;
+        return dfs(target, numbers, 0, 0);
+        
     }
     
-    public void dfs(int[] numbers, int target, int num, int idx){
+    public int dfs(int target, int[] numbers, int idx, int sum){
+        int cnt = 0;
+        
         if(idx == numbers.length){
-            if(num == target){
-                count++;
+            if(sum == target){
+                return 1;
+            }else{
+                return 0;
             }
-            return;
         }
         
-        dfs(numbers, target, num + numbers[idx], idx+1);
-        dfs(numbers, target, num - numbers[idx], idx+1);
+        cnt += dfs(target, numbers, idx + 1, sum + numbers[idx]);
+        cnt += dfs(target, numbers, idx + 1, sum - numbers[idx]);
         
+        return cnt;
     }
 }
