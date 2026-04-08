070aa5579fec0b5ef7365baaaec921c1f80c36f6
jin573 <rlawlstjs5573@gmail.com>
2026-04-08 21:26:53 +0900

    [level 2] Title: 피로도, Time: 3.00 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,47 @@
 import java.util.*;
+
 class Solution {
     int max = 0;
     public int solution(int k, int[][] dungeons) {
-        int count = 0;
+        //현재 상태, 던전으로 들어가서 계산 -> dfs 
+            // 그 후 피로도 max 구하기 
         boolean[] visited = new boolean[dungeons.length];
         
-        dfs(k, count, visited, dungeons);
+        //문제는 순서
+        for(int i = 0; i<dungeons.length; i++){
+            if(!visited[i]){
+                //최소 피로를 만족하면 k -= 소모피로도로 들어가기 
+                if(k >= dungeons[i][0]){
+                    visited[i] = true;
+                    dfs(k - dungeons[i][1], dungeons, visited, 1);
+                    visited[i] = false;
+                }
+               
+            }
+        }
+        
         return max;
     }
     
-    public void dfs(int k, int count, boolean[] visited, int[][] dungeons){
-        max = Math.max(max, count);
+    public void dfs(int k, int[][] dungeons, boolean[] visited, int cnt){
+        //언제 종료하나?
+        //최소 피로를 만족하지 않을 때
+        //반복문 안에서만 판별?
+        boolean isGo = false;
         
         for(int i = 0; i<dungeons.length; i++){
-            if(!visited[i] && k >= dungeons[i][0]){
-                visited[i] = true;
-                dfs(k - dungeons[i][1], count+1, visited, dungeons);
-                visited[i] = false;
+            if(!visited[i]){
+                if(k >= dungeons[i][0]){
+                    isGo = true;
+                    visited[i] = true;
+                    dfs(k - dungeons[i][1], dungeons, visited, cnt + 1);
+                    visited[i] = false;
+                }
             }
+        }
+        
+        if(!isGo){
+            max = Math.max(max, cnt);
         }
     }
 }
