a78afad3eea5423a833f1967804e4027dd74ec0e
jin573 <rlawlstjs5573@gmail.com>
2026-04-08 20:15:35 +0900

    [level 1] Title: 모의고사, Time: 5.54 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,40 +1,38 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
+        
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         Map<String, Integer> map = new HashMap<>();
         
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         for(int i = 0; i<answers.length; i++){
-            if(answers[i] == arr1[i % 5]) {
+            if(answers[i] == p1[i % p1.length]){
                 map.put("1", map.getOrDefault("1", 0) + 1);
             }
-            if(answers[i] == arr2[i % 8]){
+            if(answers[i] == p2[i % p2.length]){
                 map.put("2", map.getOrDefault("2", 0) + 1);
-            } 
-            if(answers[i] == arr3[i % 10]){
+            }
+            if(answers[i] == p3[i % p3.length]){
                 map.put("3", map.getOrDefault("3", 0) + 1);
-            } 
+            }
         }
-        
         int max = 0;
-        
-        for(int i : map.values()){
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i: map.values()){
             if(i >= max){
                 max = i;
             }
         }
         
-        List<Integer> arr = new ArrayList<>();
         for(String s : map.keySet()){
             if(map.get(s) == max){
                 arr.add(Integer.parseInt(s));
             }
         }
-        
-           
         return arr.stream().mapToInt(Integer::intValue).toArray();
     }
 }
