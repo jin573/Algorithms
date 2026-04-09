27d520a0bf5d15be4b234c5f83e890e51a01581d
jin573 <rlawlstjs5573@gmail.com>
2026-04-09 15:05:30 +0900

    [level 3] Title: 여행경로, Time: 0.66 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,37 +1,32 @@
 import java.util.*;
 class Solution {
-    boolean finished = false;
+    boolean isFinished = false;
     public String[] solution(String[][] tickets) {
         Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         boolean[] visited = new boolean[tickets.length];
-        ArrayList<String> arr = new ArrayList<>();
-        //icn으로 시작
-        arr.add("ICN");
-        dfs("ICN", tickets, visited, arr);
+        String[] arr = new String[tickets.length +1];
         
-        return arr.toArray(new String[0]);
+        arr[0] = "ICN";
+        dfs(arr, visited, tickets, "ICN", 1);
+        
+        return arr;
     }
     
-    public void dfs(String target, String[][] tickets, boolean[] visited,
-                   ArrayList<String> arr){
-        //언제 종료?
-        //다 돌았을 때? 
-        if(arr.size() == tickets.length+1){
-            finished = true;
+    public void dfs(String[] arr, boolean[] visited, String[][] tickets, 
+                    String str, int idx){
+
+        if(idx == arr.length) {
+            isFinished = true;
+            return;
         }
         
-        
-        
         for(int i = 0; i<tickets.length; i++){
-            //반복문을 돌면서 들어간 적 없고, 시작 경로에 해당하는 걸 찾음
-                //맞으면 target을 tickets[i][1] 로 변경함
-            if(!visited[i] && target.equals(tickets[i][0])){
+            if(!visited[i] && tickets[i][0].equals(str)){
                 visited[i] = true;
-                arr.add(tickets[i][1]);
-                dfs(tickets[i][1], tickets, visited, arr);
-                if(finished) return;
+                arr[idx] = tickets[i][1];
+                dfs(arr, visited, tickets, tickets[i][1], idx+1);
+                if(isFinished) return;
                 visited[i] = false;
-                arr.remove(arr.size() -1);
             }
         }
     }
