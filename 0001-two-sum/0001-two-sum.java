6072999985e4cecb7acdd73a05546ae068d56639
jin573 <rlawlstjs5573@gmail.com>
2026-07-01 10:19:25 +0900

    Time: 2 ms (99.36%), Space: 47 MB (57.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,10 @@
+import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         //합이 target이 되는 원소 꺼내기. 
 
         //방법 1. 배열을 돌면서 찾기*브루트포스) -> 이 경우 최악 n2이 걸릴 수 있다.
-        
+        /*
         while(true){
 
             for(int i = 0; i<nums.length-1; i++){
@@ -14,6 +15,22 @@ class Solution {
                 }
             }
         }
+        */
         //방법 2. 더 나은 방법?
+        //hashmap에 원소가 없으면 -> 넣기 
+        //있으면 -> 그 원소 반환
+        HashMap<Integer, Integer> map = new HashMap<>();//<nums[i], i>
+
+        for(int i = 0; i<nums.length; i++){
+            int idx = target - nums[i];
+
+            if(map.containsKey(idx)){
+                return new int[]{i, map.get(idx)};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{0, 0};
     }
 }
